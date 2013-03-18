c9d1f1767122b99a8961f41c736a5eb324a11ae9
Sohaib Muhammad <sohaib.muhammad@coeus-solutions.de>
2013-03-18 15:29:11 +0500

    Initial Commit

---
+++ StroyboardsPractice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StroyboardsPractice
+//
+//  Created by Sohaib Muhammad on 18/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StroyboardsPractice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StroyboardsPractice
+//
+//  Created by Sohaib Muhammad on 18/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
